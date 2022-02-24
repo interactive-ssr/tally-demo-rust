0e564e98069c702382ca388381aa7c22115697f3
Jonathan Moore <jtm170330@utdallas.edu>
2022-02-23 22:00:31 -0600

    Basic functionality (with unsafe)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use maud::{html, Markup};
+use maud::{html, Markup, DOCTYPE};
 
 use axum::{
     routing::get,
     Router, response::Html, extract::Form,
 };
 use serde::Deserialize;
+
+static mut COUNT: i32 = 0;
 
 #[tokio::main]
 async fn main() {
@@ -25,15 +27,49 @@ struct Args {
     subx: Option<String>
 }
 
-async fn tally(form: Option<Form<Args>>) -> Html<String> {
-    let markup = html! {
-        body {
-            h1 { "Tally" }
-            button onclick="rr(this)" action="addx" { "+" }
-            button onclick="rr(this)" action="subx" { "-" }
-
+fn natural_number_list(end: i32) -> Markup {
+    let range = 1..=end;
+    html! {
+        ul {
+            @for num in range {
+                li { (num) }
+            }
         }
-    };
-    Html(markup.into_string())
+    }
+}
+
+async fn tally(form: Option<Form<Args>>) -> Html<String> {
+    if let Some(form) = form {
+        let args = form.0;
+        if let Some(addx) = args.addx {
+            if !addx.is_empty() {
+                unsafe {
+                    COUNT = COUNT + 1;
+                }
+            }
+        }
+        if let Some(subx) = args.subx {
+            if !subx.is_empty() {
+                unsafe {
+                    COUNT = COUNT - 1;
+                }
+            }
+        } 
+    }
+    unsafe {
+        let count = COUNT.to_string();
+        let markup = html! {
+            (DOCTYPE)
+            head {}
+            body {
+                h1 { "Tally" }
+                button onclick="rr(this)" action="addx" { "+" }
+                button onclick="rr(this)" action="subx" { "-" }
+                (natural_number_list(COUNT))
+            }
+        };
+        Html(markup.into_string())
+    }
+
 }
 
