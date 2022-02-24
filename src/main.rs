70eeae96ab0c56c245b3ba9eae8886a2198ee27d
Jonathan Moore <jtm170330@utdallas.edu>
2022-02-23 20:42:10 -0600

    Continue refactor to Axum

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,30 @@ use maud::{html, Markup};
 
 use axum::{
     routing::get,
-    Router, response::Html,
+    Router, response::Html, extract::Form,
 };
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
         .route("/tally", get(tally).post(tally));
 
     // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-
+#[derive(Deserialize)]
 struct Args {
     addx: Option<String>,
     subx: Option<String>
 }
 
-async fn tally() -> Html<String> {
+async fn tally(form: Option<Form<Args>>) -> Html<String> {
     let markup = html! {
         body {
             h1 { "Tally" }
