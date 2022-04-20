b7ff8f0c46f810fab058b0d84ff954c4a674f0ce
Jonathan Moore <jonm@duck.com>
2022-04-19 23:59:49 -0500

    Start adding session to axum example

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
+use axum_database_sessions::{
+    axum_session_runner, AxumSession, AxumSessionConfig, AxumSessionStore, AxumDatabasePool,
+};
 use maud::{html, Markup, DOCTYPE};
 
-use axum::{
-    routing::get,
-    Router, response::Html, extract::Form,
-};
+use axum::{extract::Form, response::Html, routing::get, Router};
 use serde::Deserialize;
 
 static mut COUNT: i32 = 0;
 
 #[tokio::main]
 async fn main() {
+    // session setup
+    let session_config = AxumSessionConfig::default().with_table_name("test_table");
+    let session_store = AxumSessionStore::new(None, session_config);
+
     // build our application with a single route
     let app = Router::new()
-        .route("/tally", get(tally).post(tally));
+        .route("/tally", get(tally).post(tally))
+        .layer(axum_session_runner!(session_store))
+        .layer(tower_cookies::CookieManagerLayer::new());
 
-    // run it with hyper on localhost:3000
+    println!("Starting...");
     axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
         .serve(app.into_make_service())
         .await
@@ -24,7 +30,7 @@ async fn main() {
 #[derive(Deserialize)]
 struct Args {
     addx: Option<String>,
-    subx: Option<String>
+    subx: Option<String>,
 }
 
 fn natural_number_list(end: i32) -> Markup {
@@ -38,38 +44,33 @@ fn natural_number_list(end: i32) -> Markup {
     }
 }
 
-async fn tally(form: Option<Form<Args>>) -> Html<String> {
+async fn tally(session: AxumSession, form: Option<Form<Args>>) -> Html<String> {
+    let mut count = session.get("count").await.unwrap_or(0);
     if let Some(form) = form {
         let args = form.0;
         if let Some(addx) = args.addx {
             if !addx.is_empty() {
-                unsafe {
-                    COUNT = COUNT + 1;
-                }
+                println!("increment");
+                count += 1;
             }
         }
         if let Some(subx) = args.subx {
             if !subx.is_empty() {
-                unsafe {
-                    COUNT = COUNT - 1;
-                }
+                println!("decrement");
+                count -= 1;
             }
-        } 
+        }
+        session.set("count", count).await;
     }
-    unsafe {
-        let count = COUNT.to_string();
-        let markup = html! {
-            (DOCTYPE)
-            head {}
-            body {
-                h1 { "Tally" }
-                button onclick="rr(this)" action="addx" { "+" }
-                button onclick="rr(this)" action="subx" { "-" }
-                (natural_number_list(COUNT))
-            }
-        };
-        Html(markup.into_string())
-    }
-
+    let markup = html! {
+        (DOCTYPE)
+        head {}
+        body {
+            h1 { "Tally" }
+            button onclick="rr(this)" action="addx" { "+" }
+            button onclick="rr(this)" action="subx" { "-" }
+            (natural_number_list(0))
+        }
+    };
+    Html(markup.into_string())
 }
-
